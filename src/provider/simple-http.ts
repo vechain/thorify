d6ad253b66561a2c278c343e5f8a8cc342bd9027
tony <tony.li@vechain.com>
2018-08-30 23:26:13 +0800

    WIP(provider): bootstrap new provider

--- src/provider/simple-http.ts
+++ src/provider/simple-http.ts
@@ -1,67 +1,60 @@
 "use strict";
 const XHR2 = require("xhr2");
 
-type Method = "GET" | "POST";
-interface SimpleResponse {
-    Code: number;
-    ResponseBody: object|null;
-}
-
 // Simple HTTP is an wrapper for xhr that only support JSON format
-class SimpleHTTP {
-    private timeout: number;
-
-    constructor(timeout = 0) {
-        this.timeout = timeout;
-    }
-
-    public post(url: string, body: object) {
-        return this.request("POST", url, body);
-    }
-
-    public get(url: string) {
-        return this.request("GET", url, null);
-    }
-
-    private request(method: Method, url: string, body: object|null): Promise<SimpleResponse> {
-
-        return new Promise((resolve, reject) => {
-            const xhr = new XHR2();
-            xhr.timeout = this.timeout;
-            xhr.open(method, url);
-
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState === 4) {
-
-                    const res: SimpleResponse = {
-                        Code: xhr.status as number,
-                        ResponseBody: null,
-                    };
-
-                    if (xhr.responseText) {
-                        try {
-                            res.ResponseBody = JSON.parse(xhr.responseText);
-                        } catch (e) {
-                            return reject(new Error(`[thorify-provider]Error parsing the response: ${ e.message }`));
-                        }
-                    }
-
-                    return resolve(res);
-                }
-            };
-
-            xhr.ontimeout = () => {
-                return reject(new Error(`[thorify-provider]Time out for whatever reason, check your provider).`));
-            };
-
-            try {
-                xhr.send(method === "POST" ? JSON.stringify(body) : null);
-            } catch (e) {
-                return reject(new Error(`[thorify-provider]Connect error: ${e.message }`));
-            }
-        });
-
-    }
+export interface SimpleResponse {
+    Code: number;
+    Body: object|string;
+}
+export enum Method {
+    GET= 0,
+    POST,
 }
 
-export {SimpleHTTP};
+const post = function(url: string, body: object, timeout= 0) {
+    return request(Method.GET, url, body, timeout);
+};
+
+const get = function(url: string, timeout = 0) {
+    return request(Method.POST, url, null, timeout);
+};
+
+export const HTTP = {get, post};
+
+const request = function(method: Method, url: string, body: object | null, timeout: number): Promise<SimpleResponse> {
+
+    return new Promise((resolve, reject) => {
+        const xhr = new XHR2();
+        xhr.timeout = timeout;
+        xhr.open(method, url);
+
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4) {
+
+                let body;
+                try {
+                    body = JSON.parse(xhr.responseText);
+                } catch (e) {
+                    return reject(new Error(`[thorify-provider]Error parsing the response: ${e.message}`));
+                }
+
+                const res: SimpleResponse = {
+                    Code: xhr.status as number,
+                    Body: body,
+                };
+
+                return resolve(res);
+            }
+        };
+
+        xhr.ontimeout = () => {
+            return reject(new Error(`[thorify-provider]Time out for whatever reason, check your provider).`));
+        };
+
+        try {
+            xhr.send(method === Method.POST ? JSON.stringify(body) : null);
+        } catch (e) {
+            return reject(new Error(`[thorify-provider]Connect error: ${e.message}`));
+        }
+    });
+};
